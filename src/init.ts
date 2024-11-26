d7ace5a4e327ff1b946bf56ef6cfb60b7b466329
ytiK0 <eeeee@eee.eee>
2024-11-26 17:22:35 +0300

    remove eruda and trying to solve bg bug

--- src/init.ts
+++ src/init.ts
@@ -19,11 +19,6 @@ export function init(debug: boolean): void {
   // Also, configure the package.
   initSDK();
 
-  // Add Eruda if needed.
-  debug && import('eruda')
-    .then((lib) => lib.default.init())
-    .catch(console.error);
-
   // Check if all required components are supported.
   if (!backButton.isSupported() || !miniApp.isSupported()) {
     throw new Error('ERR_NOT_SUPPORTED');
