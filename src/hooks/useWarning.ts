f9dbb4e11990963ecd098647f649b5ab8ad62e28
ytiK0 <eeeee@eee.eee>
2025-01-31 20:52:55 +0300

    useWarning hook now can commit message

--- src/hooks/useWarning.ts
+++ src/hooks/useWarning.ts
@@ -1,21 +1,25 @@
 import {useCallback, useState} from "react";
 
 export function useWarning(delay: number | null) {
-  const [isWarningVisible, setIsWarningVisible] = useState(false)
+  const [isWarningVisible, setIsWarningVisible] = useState(false);
+  const [message, setMessage] = useState<string>();
 
-  const toggle = useCallback(() => {
+  const toggle = useCallback((message?: string) => {
     if (delay === null) {
       setIsWarningVisible((s) => !s);
+      setMessage(message);
     }
 
     else {
       setIsWarningVisible(true);
+      setMessage(message);
 
       setTimeout(() => {
         setIsWarningVisible(false);
+        setMessage(undefined);
       }, delay);
     }
   }, []);
 
-  return [isWarningVisible, toggle] as const;
+  return [isWarningVisible, toggle, message] as const;
 }
