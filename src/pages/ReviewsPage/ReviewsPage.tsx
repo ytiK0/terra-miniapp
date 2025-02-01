a3d1a6c9695dd5405e0bf90e73a59a6b2a9c911e
ytiK0 <eeeee@eee.eee>
2025-02-01 17:07:33 +0300

    use infinity scroll

--- src/pages/ReviewsPage/ReviewsPage.tsx
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -2,162 +2,13 @@ import {Page} from "@/components/Page.tsx";
 import {Logo} from "@/components/Logo/Logo.tsx";
 
 import style from "./ReviewsPage.module.css";
-import {useEffect, useRef, useState} from "react";
 import {Spinner} from "@telegram-apps/telegram-ui";
-import {getReviews, Review} from "@/api/getReviews.ts";
+import {getReviews} from "@/api/getReviews.ts";
 import {Avatar} from "@/components/Avatar/Avatar.tsx";
-
-const LIMIT = 50;
+import {useInfinityScroll} from "@/hooks/useInfinityScroll.ts";
 
 export function ReviewsPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [reviews, setReviews] = useState<Review[]>([{
-    id: 1,
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-    amount: 25,
-    user: {
-      photoURL: "",
-      name: "Utikaa"
-    }
-  },
-    {
-      id: 2,
-      text: "Lorem, ipsum dolor sit amet conserat numquam similique ratione, labore temporibus ea ullam hic ",
-      amount: 5,
-      user: {
-        photoURL: "",
-        name: "Utika2a"
-      }
-    },
-    {
-      id: 3,
-      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic mus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic mus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic mus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-      amount: 105,
-      user: {
-        photoURL: "",
-        name: "Utikaa"
-      }
-    },
-    {
-      id: 4,
-      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-      amount: 2125,
-      user: {
-        photoURL: "",
-        name: "Uti12kaa"
-      }
-    },
-    {
-      id: 5,
-      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-      amount: 25,
-      user: {
-        photoURL: "",
-        name: "Utikaa"
-      }
-    }
-  ]);
-  const [page, setPage] = useState(1);
-  const [isLast, setIsLast] = useState(false);
-  const lastRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (isLast) {
-      return;
-    }
-
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    getReviews(page, LIMIT, signal)
-      .then((reviews) => {
-        setReviews((prev) => prev.concat(reviews))
-      })
-      .then(() => setIsLoading(false))
-      .catch((er) => {throw new Error(er)})
-
-    return () => controller.abort("Load canceled")
-  }, []);
-
-  useEffect(() => {
-    if (!lastRef.current) {
-      console.log("choe")
-      return
-    }
-
-    const observer = new IntersectionObserver( async (entries) => {
-      if (entries[0].isIntersecting) {
-        await loadNextReviews();
-      }
-    }, {threshold: 0.5});
-
-    observer.observe(lastRef.current);
-
-    return () => observer.disconnect();
-  }, []);
-
-  async function loadNextReviews() {
-    console.log("loading next");
-    if (isLast) {
-      return;
-    }
-
-    console.log(lastRef);
-
-    const newReviews = await getReviews(page + 1, LIMIT);
-
-    if (newReviews.length < LIMIT) {
-      setIsLast(true);
-    }
-
-    setReviews((prev) => prev.concat([{
-      id: 6,
-      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-      amount: 25,
-      user: {
-        photoURL: "",
-        name: "22Utikaa"
-      }
-    },
-      {
-        id: 7,
-        text: "Lorem, ipsum dolor sit amet conserat numquam similique ratione, labore temporibus ea ullam hic ",
-        amount: 5,
-        user: {
-          photoURL: "",
-          name: "122Utika2a"
-        }
-      },
-      {
-        id: 8,
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic mus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic mus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic mus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-        amount: 105,
-        user: {
-          photoURL: "",
-          name: "222Utikaa"
-        }
-      },
-      {
-        id: 9,
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-        amount: 2125,
-        user: {
-          photoURL: "",
-          name: "322Uti12kaa"
-        }
-      },
-      {
-        id: 10,
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum enim repellat usamus rerum obcaecati distinctio cupiditate quaerat numquam similique ratione, labore temporibus ea ullam hic ",
-        amount: 25,
-        user: {
-          photoURL: "",
-          name: "422Utikaa"
-        }
-      }
-    ]));
-    setPage((prev) => prev++);
-  }
+  const [spinnerRef, reviews, isLast] = useInfinityScroll(getReviews);
 
   return (
     <Page>
@@ -166,7 +17,7 @@ export function ReviewsPage() {
         Reviews
       </header>
       <section className={style.reviewsWrapper}>
-        {!isLoading && reviews.length === 0 ? <span>No reviews to show</span>
+        { reviews.length === 0 ? <span style={{color: "gray"}}>No reviews to show</span>
             : reviews.map(({id, text, user, amount}) => (
               <div key={id} className={style.reviewBox}>
                 <div className={style.reviewHeaderWrapper}>
@@ -186,7 +37,7 @@ export function ReviewsPage() {
             ))
         }
         { isLast ||
-            <div ref={lastRef}>
+            <div ref={spinnerRef}>
               <Spinner size={"l"}/>
             </div>
         }
--- src/pages/WithdrawListPage/WithdrawListPage.tsx
+++ src/pages/WithdrawListPage/WithdrawListPage.tsx
@@ -1,13 +1,15 @@
 import style from "./WithdrawListPage.module.css"
 import {Page} from "@/components/Page.tsx";
 import {Logo} from "@/components/Logo/Logo.tsx";
-import {useLoading} from "@/hooks/useLoading.ts";
 import {getCashOuts} from "@/api/getCashOuts.ts";
-import {Spinner} from "@telegram-apps/telegram-ui";
 import {Avatar} from "@/components/Avatar/Avatar.tsx";
+import {useInfinityScroll} from "@/hooks/useInfinityScroll.ts";
+import {Spinner} from "@telegram-apps/telegram-ui";
+
+const padDate = (component: number) => component.toString().padStart(2, "0");
 
 export function WithdrawListPage() {
-  const {isLoading, loadData: cashOuts} = useLoading(getCashOuts);
+  const [spinnerRef, data, isLast] = useInfinityScroll(getCashOuts);
 
   return (
     <Page back={true}>
@@ -18,19 +20,18 @@ export function WithdrawListPage() {
         </span>
       </header>
       <section className={style.withdrawSection}>
-        { isLoading ? <Spinner size={"l"} /> :
-            cashOuts && cashOuts.length !== 0 ?
-              cashOuts.map((cashOut, i, couts) => {
+        { data.length !== 0 ?
+              data.map((cashOut, i, couts) => {
                 const date = new Date(cashOut.completedAt);
                 const user = cashOut.user;
 
-                return <>
+                return <div key={cashOut.id}>
                   { i !== 0 && (new Date(couts[i - 1]?.completedAt)).getDate() !== date.getDate() ?
                     <div className={style.divideLine}></div> : null
                   }
-                  <div style={{textAlign: "left"}} key={cashOut.id}>
+                  <div style={{textAlign: "left"}}>
                     <span className={style.time}>
-                      {date.getDate()}.{date.getMonth()} {date.getHours()}:{date.getMinutes()}
+                      {date.getDate()}.{padDate(date.getMonth()+1)} {date.getHours()}:{padDate(date.getMinutes())}
                     </span>
                     <div className={style.withdrawElementWrapper}>
                       <Avatar imgUrl={cashOut.user.photoURL} alt={"userpic"} width={50}/>
@@ -44,11 +45,17 @@ export function WithdrawListPage() {
                         <span>{cashOut.amount}$</span>
                       </div>
                       </div>
+                    </div>
                   </div>
-                  </>
                 }
               ) :
               <span style={{color: "gray"}}>No cash-outs yet</span>
+        }
+        {
+          isLast ||
+            <div ref={spinnerRef}>
+                <Spinner size={"l"} />
+            </div>
         }
       </section>
     </Page>
