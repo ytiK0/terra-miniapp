ab97da80762c283b5c729ebee9fa5688ef941a82
ytiK0 <eeeee@eee.eee>
2025-03-28 14:16:05 +0300

    eruda added for clientside debugging

--- src/init.ts
+++ src/init.ts
@@ -7,11 +7,13 @@ import {
   $debug,
   init as initSDK,
 } from '@telegram-apps/sdk-react';
+import eruda from "eruda";
 
 /**
  * Initializes the application and configures its dependencies.
  */
 export function init(debug: boolean): void {
+  eruda.init()
   // Set @telegram-apps/sdk-react debug mode.
   $debug.set(debug);
 
