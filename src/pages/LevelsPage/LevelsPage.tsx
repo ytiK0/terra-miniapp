2add71d60772c6bcf3d9045a5f234988516e9825
ytiK0 <eeeee@eee.eee>
2025-03-31 14:45:00 +0300

    added infinity icons; fix hold days counting formula

--- src/pages/LevelsPage/LevelsPage.tsx
+++ src/pages/LevelsPage/LevelsPage.tsx
@@ -7,10 +7,20 @@ import {Badge} from "@/components/Badge/Badge.tsx";
 import {getStatus} from "@/helpers/getStatus.ts";
 import {calcLevel, levels} from "@/helpers/calcLevel.ts";
 import {useAppStore} from "@/state/appState.ts";
-import {useMemo} from "react";
+import {FC, useMemo} from "react";
 import {binarySearch} from "@/helpers/binarySearch.ts";
 
-export const statusStats: Record<string, {gain: string, level: string, gainNum: number}> = {
+const InfinityIcon: FC<{ color: string, align: string, height: number }> = ({color, align, height}) => (
+  <svg viewBox="0 0 24 24" height={height} style={{verticalAlign: align}} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M18.1777 8C23.2737 8 23.2737 16 18.1777 16C13.0827 16 11.0447 8 5.43875 8C0.85375 8 0.85375 16 5.43875 16C11.0447 16 13.0828 8 18.1788 8H18.1777Z"
+        stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+    </g>
+  </svg>
+)
+
+export const statusStats: Record<string, { gain: string, level: string, gainNum: number }> = {
   "Young": {
     gainNum: 0.5,
     gain: "+0.5% +10T",
@@ -41,6 +51,7 @@ export const statusStats: Record<string, {gain: string, level: string, gainNum: 
 const DAYS_COUNTING_PERCENT = 1.04
 
 export function LevelsPage() {
+  const usdt = useAppStore((s) => s.userWallet.usdt);
   const coins = useAppStore((s) => s.userWallet.terroCoins);
 
 
@@ -71,10 +82,10 @@ export function LevelsPage() {
 
   const holdDays = useMemo(() => {
     if (!isLastLevel && coins !== 0) {
-      return binarySearch((mid) => levels[levelIndex+1].coins >= coins * DAYS_COUNTING_PERCENT ** mid, 1, 5 * 365);
+      return binarySearch((mid) => (levels[levelIndex+1].coins * 2) >= (usdt) * DAYS_COUNTING_PERCENT ** mid, 1, 5 * 365);
     }
 
-    return "inf";
+    return <InfinityIcon height={18} color={"#D5D5D599"} align={"bottom"} />;
   }, [coins])
 
   return (
@@ -96,10 +107,10 @@ export function LevelsPage() {
           <div className={styles.progressBar}>
             <div className={styles.progressBarFiller} style={{width: `${progressPercent}%`, height: "100%"}}></div>
           </div>
-          <span>{isLastLevel ? "inf" : currentLevel + 1 }</span>
+          <span>{isLastLevel ? <InfinityIcon height={30} align={"top"} color={"white"} /> : currentLevel + 1 }</span>
         </div>
         <span className={styles.progressHint}>
-          {holdDays} days hold / Deposit {coinsToNextLevel}$
+          {holdDays} days hold / Deposit {coinsToNextLevel * 2}$
         </span>
       </section>
 
