5d9626d63f35ce9a8777c10351012fe0771c3b89
ytiK0 <eeeee@eee.eee>
2025-04-03 14:34:49 +0300

    fix: user map types fix

--- src/api/getTopUsers.ts
+++ src/api/getTopUsers.ts
@@ -3,5 +3,5 @@ import {UserBackend} from "@/api/getUserWallet.ts";
 export async function getTopUsers(signal?: AbortSignal) {
   return fetch(`${import.meta.env.VITE_TERRA_API_BASEURL}/user/findTop`, {signal})
     .then((res) => res.json() as Promise<UserBackend[]>)
-    .then((topList) => topList.map((user) => ({name: user.name, coins: user.coins, id: user.telegramId, photoURL: user.photoURL})));
+    .then((topList) => topList.map((user) => ({name: user.name, coins: +(+user.coins).toFixed(2), id: user.telegramId, photoURL: user.photoURL})));
 }
